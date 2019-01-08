777e0c46b1ee88cbee2d88049927e6f74a2f2dc2
nguyenphuongtran07 <46473871+nguyenphuongtran07@users.noreply.github.com>
2019-01-08 18:43:40 +0700

    Add files via upload

---
+++ phuongtrann.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int ThoiHan;
+	float TienGui, TienLai, WishMoney, LaiSuat, Sum, count = 1;
+	cout << "Nhap vao so tien gui ban dau : " << endl;
+	cin >> TienGui;
+	cout << "Nhap vao so tien ky vong : " << endl;
+	cin >> WishMoney;
+	cout << "Nhap vao lai suat khi gui (%): " << endl;
+	cin >> LaiSuat;
+
+	TienLai = (TienGui * LaiSuat) / 100;
+	Sum = TienGui + TienLai;
+
+	do
+	{
+		if (Sum < WishMoney)
+		{
+			count++;
+			Sum = Sum + TienLai;
+		}
+		else
+			break;
+	} while (1 != 2);
+	cout << "Thoi han dat duoc so tien " << WishMoney << " la : " << count << " nam." << endl;
+	return 0;
+}
